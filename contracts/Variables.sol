60c67a8c53ed0275eb33a1a2548abeb7a9f83059
"priyatham" <"priyatham002@gmail.com">
2022-06-27 12:27:51 +0100

    Basics started

---
+++ contracts/Variables.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+// Version for solidity
+pragma solidity >=0.4.22 <0.9.0;
+
+
+// contract for starting the smart contract instance
+contract variablespractice{
+
+
+
+}
